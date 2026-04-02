e731de33abb2cf041b9b3deba436193a81410983
nazneenlipi <lipinazneen65@gmail.com>
2026-04-02 23:55:22 +0600

    feat: test

--- apps/web/app/styles.tsx
+++ apps/web/app/styles.tsx
@@ -17,11 +17,3 @@ export const Main = styled.main`
   align-items: center;
   justify-content: center;
 `;
-
-export const Footer = styled.footer`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 1rem;
-  background: ${({ theme }) => theme.colors.neutral.GRAY_100};
-`;
