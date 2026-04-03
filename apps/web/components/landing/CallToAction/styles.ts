b1ff31dbd03be87dffcffa880c3290a0586ef090
seraprogrammer <programmernazmul1@gmail.com>
2026-04-03 17:24:05 +0600

    fix: fix height

--- apps/web/components/landing/CallToAction/styles.ts
+++ apps/web/components/landing/CallToAction/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Section = styled.section`
   position: relative;
   width: 100%;
+  height: 100vh;
   margin: 0;
   overflow: hidden;
   background: #06210f;
