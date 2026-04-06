4ff4b2cf4e6eae13830fd8d3d514f146b1ce3d5e
Farjana Akter Laila <farjanaakter7275@gmail.com>
2026-04-06 19:47:04 +0600

    fix: fixed page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <HeroSection />
         <TestimonialSection />
       </Main>
-
+      
       <WatchingSteps />
       <ValueStatement />
       <CallToAction />
