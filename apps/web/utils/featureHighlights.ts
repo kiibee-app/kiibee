eba15614e1f536c77a657d87759ea0d49ca024c2
Muksana <muksanaakter3@gmail.com>
2026-04-06 20:31:55 +0600

    feat: section data & color updated

---
+++ apps/web/utils/featureHighlights.ts
@@ -0,0 +1,26 @@
+import mockImg from "../assets/images/steps/content.png";
+
+export const featureData = [
+  {
+    titleKey: "features.items.0.title",
+    textKey: "features.items.0.text",
+    image: mockImg,
+  },
+  {
+    titleKey: "features.items.1.title",
+    textKey: "features.items.1.text",
+    image: mockImg,
+  },
+  {
+    titleKey: "features.items.2.title",
+    textKey: "features.items.2.text",
+    image: mockImg,
+  },
+  {
+    titleKey: "features.items.3.title",
+    textKey: "features.items.3.text",
+    image: mockImg,
+  },
+];
+
+export default featureData;
