2784ffdee8cc1d55127a727a51f3ef5d3a7b131f
Muksana <muksanaakter3@gmail.com>
2026-04-06 15:06:56 +0600

    feat: short story section

--- apps/web/app/(pages)/how-it-works/page.tsx
+++ apps/web/app/(pages)/how-it-works/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import NavBar from "@/components/Layout/Navbar";
 import { PageContainer, Main, Section } from "../../styles";
 import HowHero from "@/components/Feature/HowItWork/Hero";
+import CustomerSection from "@/components/Feature/HowItWork/CustomerSection";
 import HowSteps from "@/components/Feature/HowItWork/Steps";
 
 export default function HowItWorksPage() {
@@ -14,6 +15,7 @@ export default function HowItWorksPage() {
         <Section>
           <HowHero />
           <HowSteps />
+          <CustomerSection />
         </Section>
       </Main>
     </PageContainer>
