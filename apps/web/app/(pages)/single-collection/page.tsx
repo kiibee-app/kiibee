d25b99de6ce2a3573a959bf4b1f9a538321cf36a
Farjana Akter Laila <farjanaakter7275@gmail.com>
2026-04-10 21:48:57 +0600

    fix: fixed build

--- apps/web/app/(pages)/single-collection/page.tsx
+++ apps/web/app/(pages)/single-collection/page.tsx
@@ -1,24 +1,43 @@
 "use client";
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import NavBar from "@/components/Layout/Navbar";
 import Footer from "@/components/Layout/Footer";
 import { PageContainer, Main, Section } from "../../styles";
 import { tutorialVideoSections } from "@/utils/data";
 import SingleCollectionHero from "@/components/Feature/SingleCollectionHero";
+import { MonoText } from "@/components/UI/Monotext";
+import { useTranslation } from "react-i18next";
 
-export default function SingleCollectionPage() {
+function SingleCollectionContent() {
+  const { t } = useTranslation();
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
-  const section = tutorialVideoSections.find((s) => s.id === id)!;
 
+  const section = tutorialVideoSections.find((s) => s.id === id);
+
+  if (!section) {
+    return (
+      <MonoText $use="H5_Regular">{t("singleCollection.notFound")}</MonoText>
+    );
+  }
+
+  return (
+    <Section>
+      <SingleCollectionHero title={section.title} />
+    </Section>
+  );
+}
+
+export default function SingleCollectionPage() {
   return (
     <PageContainer>
       <NavBar />
       <Main>
-        <Section>
-          <SingleCollectionHero title={section.title} />
-        </Section>
+        <Suspense>
+          <SingleCollectionContent />
+        </Suspense>
       </Main>
       <Footer />
     </PageContainer>
