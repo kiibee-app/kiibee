4d2834b57ab713f3241905d34f3a3beefb98d450
Urmi Rahman <urmi.rahman690@gmail.com>
2026-04-10 17:54:03 +0600

    feat: refactored

--- apps/web/app/(pages)/creators/page.tsx
+++ apps/web/app/(pages)/creators/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Layout/Footer";
 import { PageContainer, Main } from "../../styles";
 import CreatorsSection from "./index";
 import ShortStory from "./creators/ShortStory";
-import WhyCreatorsChoose from "./creators/WhyCreatorsChoose";
+import WhyCreatorsChoose from "../../../components/Feature/ExploreCreators/Creators/WhyCreatorsChoose";
 
 export default function CreatorsPage() {
   return (
