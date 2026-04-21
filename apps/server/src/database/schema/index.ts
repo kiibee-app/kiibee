10f59832f72a0c32da2d7b8ecb9667d3039a7fbc
biplob2358 <biplob2358@gmail.com>
2026-04-21 19:37:13 +0600

    feat: create Creator request API

--- apps/server/src/database/schema/index.ts
+++ apps/server/src/database/schema/index.ts
@@ -3,3 +3,4 @@ export * from './users/userProfiles.schema';
 export * from './users/userSessions.schema';
 export * from './users/accountSetupTokens.schema';
 export * from './users/creatorApplicationRequests.schema';
+export * from './creator/creatorInfo.schema';
---
+++ apps/server/src/utils/constant.ts
@@ -0,0 +1,12 @@
+export const STATUS = {
+  PENDING: 'pending',
+  APPROVED: 'approved',
+  REJECTED: 'rejected',
+  PENDING_SETUP: 'pending-setup',
+} as const;
+
+export const ROLE = {
+  VIWER: 'viewer',
+  CREATOR: 'creator',
+  ADMIN: 'admin',
+} as const;
