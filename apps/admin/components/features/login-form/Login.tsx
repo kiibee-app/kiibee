950032b3373fc932210375dbc01eeccea5bb2935
seraprogrammer <programmernazmul1@gmail.com>
2026-04-29 14:42:55 +0600

    fix: separate svg

--- apps/admin/components/features/login-form/Login.tsx
+++ apps/admin/components/features/login-form/Login.tsx
@@ -3,6 +3,7 @@
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 import AdminLogoIcon from "../../../assets/icons/AdminLogoIcon";
+import ErrorIcon from "../../../assets/icons/ErrorIcon";
 import {
   Card,
   ErrorBox,
@@ -90,21 +91,7 @@ export default function Login() {
 
           {error ? (
             <ErrorBox>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <circle cx="12" cy="12" r="10" />
-                <line x1="12" x2="12" y1="8" y2="12" />
-                <line x1="12" x2="12.01" y1="16" y2="16" />
-              </svg>
+              <ErrorIcon />
               <ErrorText>{error}</ErrorText>
             </ErrorBox>
           ) : null}
