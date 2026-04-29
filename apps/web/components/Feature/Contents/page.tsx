43e4389f7b40d85840872fb05fc818f93c19a502
Muksana <muksanaakter3@gmail.com>
2026-04-29 14:09:46 +0600

    feat: dynamic content header

--- apps/web/components/Feature/Contents/page.tsx
+++ apps/web/components/Feature/Contents/page.tsx
@@ -2,15 +2,12 @@
 
 import React, { useState } from "react";
 import { SearchIcon } from "@/assets/icons/searchBarIcon";
-import { PlusIcon } from "@/assets/icons/PlusIcon";
 
-import { MonoText } from "@/components/UI/Monotext";
 import { useTranslation } from "react-i18next";
 import { GenericModal } from "@/components/UI/Modals";
 import COLORS from "@repo/ui/colors";
 import {
   ContentPanel,
-  CreateButton,
   PageHeader,
   PageShell,
   PlaceholderLine,
@@ -27,12 +24,17 @@ import {
 } from "@/utils/common";
 import AdmissionRequirements from "./AdmissionRequirements";
 import { SuccessArcIcon } from "@/assets/icons";
+import ContentsHeaderAction from "./ContentsHeaderAction";
 
 export default function CreatorsContents() {
   const { t } = useTranslation();
   const [activeTab, setActiveTab] = useState<ContentTab>(COLLECTIONS);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [showDeleteSuccessModal, setShowDeleteSuccessModal] = useState(false);
+
+  // TODO: wire these up when appearance/settings become editable forms.
+  const handleCancel = () => {};
+  const handleSave = () => {};
 
   const handleDeleteContent = () => {
     setShowDeleteModal(false);
@@ -43,13 +45,11 @@ export default function CreatorsContents() {
     <PageShell>
       <PageHeader>
         <Title>{t("contents.title")}</Title>
-
-        <CreateButton type="button">
-          <PlusIcon width={16} height={16} color="white" />
-          <MonoText $use="Body_Medium" color="inherit">
-            {t("contents.actions.createCollection")}
-          </MonoText>
-        </CreateButton>
+        <ContentsHeaderAction
+          activeTab={activeTab}
+          onCancel={handleCancel}
+          onSave={handleSave}
+        />
       </PageHeader>
 
       <TabsRow>
