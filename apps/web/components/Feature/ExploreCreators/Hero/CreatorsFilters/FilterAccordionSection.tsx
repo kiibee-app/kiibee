054390a26e8e7f4a5362fc481018dae36f3cc5e3
Farjana Akter Laila <farjanaakter7275@gmail.com>
2026-05-07 19:13:16 +0600

    fix: refactor on export creator paage

---
+++ apps/web/components/Feature/ExploreCreators/Hero/CreatorsFilters/FilterAccordionSection.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import {
+  FilterSection,
+  FilterSectionBody,
+  FilterSectionBodyInner,
+  FilterSectionButton,
+  FilterSectionTitle,
+} from "../styles";
+import { BUTTON } from "@/utils/Constants";
+
+type FilterAccordionSectionProps = {
+  title: string;
+  isOpen: boolean;
+  onToggle: () => void;
+  children: React.ReactNode;
+  icon: React.ReactNode;
+};
+
+export default function FilterAccordionSection({
+  title,
+  isOpen,
+  onToggle,
+  children,
+  icon,
+}: FilterAccordionSectionProps) {
+  return (
+    <FilterSection>
+      <FilterSectionButton
+        type={BUTTON}
+        onClick={onToggle}
+        aria-expanded={isOpen}
+      >
+        <FilterSectionTitle>{title}</FilterSectionTitle>
+        {icon}
+      </FilterSectionButton>
+      <FilterSectionBody $open={isOpen}>
+        <FilterSectionBodyInner $open={isOpen}>
+          {children}
+        </FilterSectionBodyInner>
+      </FilterSectionBody>
+    </FilterSection>
+  );
+}
