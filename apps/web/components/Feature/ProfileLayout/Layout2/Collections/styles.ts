1d9f9d352914bcbcd51d3d26490ec55386652eb2
nhwnahid007 <nhwnahid@gmail.com>
2026-05-18 15:03:56 +0600

    fix: #712: Stop cross-importing CollectionList between ProfileLayout variants

--- apps/web/components/Feature/ProfileLayout/Layout2/Collections/styles.ts
+++ apps/web/components/Feature/ProfileLayout/Layout2/Collections/styles.ts
@@ -51,16 +51,3 @@ export const CollectionCardSlot = styled.div`
   width: 100%;
   min-width: 0;
 `;
-
-export const CollectionListShell = styled.section`
-  width: 100%;
-  max-width: 1320px;
-  margin: 0 auto;
-  padding: 35px 0 100px 0;
-  box-sizing: border-box;
-  background-color: ${({ theme }) => theme.colors.primary.WHITE};
-
-  ${media.tablet} {
-    padding-top: 28px;
-  }
-`;
