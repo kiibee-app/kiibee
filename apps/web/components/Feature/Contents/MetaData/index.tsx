58931c7012e0d0ff25eac0b48a81211e1dc83a1b
Farjana Akter Laila <farjanaakter7275@gmail.com>
2026-05-22 16:05:17 +0600

    feat: added thumbnail upload

--- apps/web/components/Feature/Contents/MetaData/index.tsx
+++ apps/web/components/Feature/Contents/MetaData/index.tsx
@@ -3,13 +3,38 @@
 import React from "react";
 import { PanelStack } from "./styles";
 import DescriptionSection from "../Appearance/Description";
+import CoverImageSection from "../Appearance/CoverImage";
+import { IMAGE_TYPE } from "@/utils/ui";
+import { useTranslation } from "react-i18next";
 import ProductionSection from "./Production";
 
 export default function MetaData() {
+  const { t } = useTranslation();
+
   return (
     <PanelStack>
       <DescriptionSection showTitle={true} />
       <ProductionSection />
+      <CoverImageSection
+        title={t("contents.metadata.coverImage.title")}
+        subtitle={true}
+        uploadConfigs={[
+          {
+            label: t("contents.metadata.coverImage.mediaCardLabel"),
+            sizeText: t("contents.metadata.coverImage.mediaCardSize"),
+            cropWidth: 250,
+            cropHeight: 190,
+            type: IMAGE_TYPE.MEDIA_CARD,
+          },
+          {
+            label: t("contents.metadata.coverImage.portraitLabel"),
+            sizeText: t("contents.metadata.coverImage.portraitSize"),
+            cropWidth: 634,
+            cropHeight: 345,
+            type: IMAGE_TYPE.PORTRAIT,
+          },
+        ]}
+      />
     </PanelStack>
   );
 }
