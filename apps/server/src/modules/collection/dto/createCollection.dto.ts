8c1cfb18e7d61c2ce6cd70d436e3ddde56fd5c7b
Farjana Akter Laila <farjanaakter7275@gmail.com>
2026-06-08 18:58:17 +0600

    fix: fixed type

--- apps/server/src/modules/collection/dto/createCollection.dto.ts
+++ apps/server/src/modules/collection/dto/createCollection.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsBoolean,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCollectionDto {
   @IsString()
@@ -13,45 +7,25 @@ export class CreateCollectionDto {
 }
 
 export class UpdateCollectionDto {
-  @IsOptional()
   @IsString()
   name?: string;
 
-  @IsOptional()
   @IsString()
   coverImageUrl?: string;
 
-  @IsOptional()
   visibility?: 'public' | 'hidden' | 'draft' | 'private';
 
-  @IsOptional()
   accessType?: 'free' | 'paid' | 'password' | 'email_gated';
 
-  @IsOptional()
-  @IsNumber()
   buyPrice?: number;
 
-  @IsOptional()
-  @IsNumber()
   rentPrice?: number;
 
-  @IsOptional()
-  @IsString()
   rentDuration?: string;
 
-  @IsOptional()
-  @IsString()
   description?: string;
 
-  @IsOptional()
-  @IsString()
-  password?: string;
-
-  @IsOptional()
-  @IsNumber()
   sortOrder?: number;
 
-  @IsOptional()
-  @IsBoolean()
   isPublished?: boolean;
 }
