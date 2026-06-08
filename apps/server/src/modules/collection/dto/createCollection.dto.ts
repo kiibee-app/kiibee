77c76c5a1628c5cd037150e3094a69a82acec931
Farjana Akter Laila <farjanaakter7275@gmail.com>
2026-06-08 19:10:33 +0600

    fix: fixed build

--- apps/server/src/modules/collection/dto/createCollection.dto.ts
+++ apps/server/src/modules/collection/dto/createCollection.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCollectionDto {
   @IsString()
@@ -29,7 +29,6 @@ export class UpdateCollectionDto {
 
   isPublished?: boolean;
 
-  @IsOptional()
   @IsString()
   password?: string;
 }
