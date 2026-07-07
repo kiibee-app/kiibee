917a7aa85992f2eaf464b541063974f4d8cc9dab
nhwnahid007 <nhwnahid@gmail.com>
2026-07-07 19:22:38 +0600

    style: fix creator and viewer height mismatch

--- apps/web/components/Layout/Sidebar/styles.ts
+++ apps/web/components/Layout/Sidebar/styles.ts
@@ -41,8 +41,7 @@ export const SidebarMenu = styled.div<{ $expanded?: boolean }>`
   display: flex;
   flex-direction: column;
   flex-shrink: 0;
-  padding: ${({ $expanded }) =>
-    $expanded ? "53px 19px 16px" : "53px 8px 16px"};
+  padding: ${({ $expanded }) => ($expanded ? "30px 19px" : "30px 16px")};
   gap: 16px;
 `;
 
