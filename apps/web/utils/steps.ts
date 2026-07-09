0afea7f3be08a33d1661c50d4e2d3891f226d5e2
seraprogrammer <programmernazmul1@gmail.com>
2026-07-09 18:26:35 +0600

    refactor: image needs to change per Figma in Localhost section

--- apps/web/utils/steps.ts
+++ apps/web/utils/steps.ts
@@ -1,4 +1,10 @@
-import dashboardImage from "@/assets/images/creator-dashboard.png";
+import dashboardImage from "@/assets/images/creator-dashboard.webp";
+import creatorRequestAccess from "@/assets/images/creator-request-access.webp";
+import creatorApproved from "@/assets/images/creator-approved.webp";
+import creatorCollections from "@/assets/images/creator-collections.webp";
+import creatorAnalytics from "@/assets/images/creator-analytics.webp";
+import creatorPayouts from "@/assets/images/creator-payouts.webp";
+import creatorProfile from "@/assets/images/creator-profile.webp";
 import step1 from "../assets/images/steps/step1.webp";
 import step2 from "../assets/images/steps/step2.webp";
 import step3 from "../assets/images/steps/step3.webp";
@@ -13,13 +19,13 @@ export const creatorOnboardingSteps: CreatorOnboardingStep[] = [
     id: 1,
     titleKey: "creators.howToGetStarted.steps.requestAccess.title",
     descriptionKey: "creators.howToGetStarted.steps.requestAccess.description",
-    image: dashboardImage,
+    image: creatorRequestAccess,
   },
   {
     id: 2,
     titleKey: "creators.howToGetStarted.steps.getApproved.title",
     descriptionKey: "creators.howToGetStarted.steps.getApproved.description",
-    image: dashboardImage,
+    image: creatorApproved,
   },
   {
     id: 3,
@@ -33,28 +39,28 @@ export const creatorOnboardingSteps: CreatorOnboardingStep[] = [
     descriptionKey:
       "creators.howToGetStarted.steps.contentCollections.description",
     listKey: "creators.howToGetStarted.steps.contentCollections.list",
-    image: dashboardImage,
+    image: creatorCollections,
   },
   {
     id: 5,
     titleKey: "creators.howToGetStarted.steps.usersSales.title",
     descriptionKey: "creators.howToGetStarted.steps.usersSales.description",
     listKey: "creators.howToGetStarted.steps.usersSales.list",
-    image: dashboardImage,
+    image: creatorAnalytics,
   },
   {
     id: 6,
     titleKey: "creators.howToGetStarted.steps.settingsPayouts.title",
     descriptionKey:
       "creators.howToGetStarted.steps.settingsPayouts.description",
-    image: dashboardImage,
+    image: creatorPayouts,
   },
   {
     id: 7,
     titleKey: "creators.howToGetStarted.steps.profileSettings.title",
     descriptionKey:
       "creators.howToGetStarted.steps.profileSettings.description",
-    image: dashboardImage,
+    image: creatorProfile,
   },
 ];
 
