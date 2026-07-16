14a08dd05d5caa0d530556d1f27425924bf2a247
rafimd422 <rafimd2222@gmail.com>
2026-07-16 19:43:19 +0600

    feat: added email service on creator deletion approval

--- apps/server/src/utils/mailServiceConstant.ts
+++ apps/server/src/utils/mailServiceConstant.ts
@@ -1,11 +1,13 @@
 export const templateName = {
   APPROVED_CREATOR: 'approvedCreator',
+  APPROVED_CREATOR_DELETION: 'approvedCreatorDeletion',
   REJECTED_CREATOR: 'rejectedCreator',
   RESET_PASSWORD: 'resetPassword',
 };
 
 export const mailSubject = {
   APPROVED_CREATOR: 'Your Kiibee Creator Request Has Been Approved 🎉',
+  APPROVED_CREATOR_DELETION: 'Your Kiibee Account Has Been Deleted',
   REJECTED_CREATOR: 'Update on Your Kiibee Creator Application',
   RESET_PASSWORD: 'Reset Your Kiibee Password',
 };
