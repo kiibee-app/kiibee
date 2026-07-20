adc79f3b4f7f0b65787abb47e2ae19047cae97bf
Urmi Rahman <urmi.rahman690@gmail.com>
2026-07-20 18:18:20 +0600

    Update styled.d.ts

--- packages/ui/src/styled.d.ts
+++ packages/ui/src/styled.d.ts
@@ -2,6 +2,5 @@ import "styled-components";
 import type { Theme } from "./theme";
 
 declare module "styled-components" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends Theme {}
 }
