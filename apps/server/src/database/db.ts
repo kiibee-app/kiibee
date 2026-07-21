76b6d9760a7e1a0cbf88dc2b26fbbb971ebf7e22
biplob2358 <biplob2358@gmail.com>
2026-07-21 13:38:29 +0600

    fix: and const

--- apps/server/src/database/db.ts
+++ apps/server/src/database/db.ts
@@ -2,11 +2,12 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema';
 import { env } from 'src/config/env';
+import { ENVIRONMENT } from 'src/utils/constant';
 
 export const pool = new Pool({
   connectionString: env.DATABASE_URL,
   ssl:
-    env.NODE_ENV === 'production'
+    env.NODE_ENV === ENVIRONMENT.PRODUCTION
       ? true
       : {
           rejectUnauthorized: false,
