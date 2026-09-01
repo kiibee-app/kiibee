77e7dce1ba8043884d0e9bd337122a41cbd103ca
nazneenlipi <lipinazneen65@gmail.com>
2026-09-01 19:02:22 +0600

    fix: update img size

--- apps/web/utils/ui.ts
+++ apps/web/utils/ui.ts
@@ -202,14 +202,14 @@ export const IMAGE_TYPE = {
 export type ImageType = (typeof IMAGE_TYPE)[keyof typeof IMAGE_TYPE];
 
 export const CONTENT_THUMBNAIL_SIZE = {
-  MEDIA_CARD: { width: 300, height: 424 },
-  PORTRAIT: { width: 1920, height: 1080 },
+  MEDIA_CARD: { width: 250, height: 190 },
+  PORTRAIT: { width: 376, height: 530 },
   PORTRAIT_PDF: { width: 376, height: 530 },
 } as const;
 
 export const CONTENT_THUMBNAIL_PREVIEW = {
-  MEDIA_CARD: { maxWidth: "71px", minHeight: "100px" },
-  PORTRAIT: { maxWidth: "184px", minHeight: "100px" },
+  MEDIA_CARD: { maxWidth: "131px", minHeight: "100px" },
+  PORTRAIT: { maxWidth: "71px", minHeight: "100px" },
   PORTRAIT_PDF: { maxWidth: "71px", height: "100px", minHeight: "100px" },
 } as const;
 
@@ -268,22 +268,22 @@ export const previewConfig: Record<ImageType, PreviewStyleConfig> = {
   },
 
   [IMAGE_TYPE.MEDIA_CARD]: {
-    maxWidth: "71px",
+    maxWidth: "131px",
     minHeight: "100px",
     aspectRatio: `${CONTENT_THUMBNAIL_SIZE.MEDIA_CARD.width} / ${CONTENT_THUMBNAIL_SIZE.MEDIA_CARD.height}`,
     tablet: {
-      maxWidth: "55px",
-      minHeight: "78px",
+      maxWidth: "100px",
+      minHeight: "76px",
     },
   },
 
   [IMAGE_TYPE.PORTRAIT]: {
-    maxWidth: "184px",
+    maxWidth: "71px",
     minHeight: "100px",
     aspectRatio: `${CONTENT_THUMBNAIL_SIZE.PORTRAIT.width} / ${CONTENT_THUMBNAIL_SIZE.PORTRAIT.height}`,
     tablet: {
-      maxWidth: "140px",
-      minHeight: "79px",
+      maxWidth: "55px",
+      minHeight: "78px",
     },
   },
 };
